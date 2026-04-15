70ccaf5169d95b761ae919099076b67f3cb20948
EmilianoMt <emiliano.portillo1331@gmail.com>
2026-04-15 12:53:05 -0600

    feat: VehicleCard component and 3-column catalog grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
+import { CatalogGrid } from "./components/CatalogGrid";
 import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero/>
-      <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl font-bold">Configuracion Inicial!</h1>
-      </div>
+      <CatalogGrid/>
+      
     </>
   );
 }
