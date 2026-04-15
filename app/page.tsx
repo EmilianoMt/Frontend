824a46c7fa8df60d5b87dea181b41d8b74d67750
EmilianoMt <emiliano.portillo1331@gmail.com>
2026-04-15 14:37:51 -0600

    feat: banner section with visual patterns

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { CatalogGrid } from "./components/CatalogGrid";
 import { Hero } from "./components/Hero";
+import { PromoSection } from "./components/PromoSection";
 
 export default function Home() {
   return (
     <>
       <Hero/>
       <CatalogGrid/>
-      
+      <PromoSection/>
     </>
   );
 }
