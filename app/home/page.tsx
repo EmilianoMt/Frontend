922f6c9f772c91f65a06fb401a13a55383094e12
EmilianoMt <emiliano.portillo1331@gmail.com>
2026-04-16 13:20:15 -0600

    Fix: The navigation on the home page has been fixed

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import { CatalogGrid } from "../components/CatalogGrid";
 import { Hero } from "../components/Hero";
 import { PromoSection } from "../components/PromoSection";
@@ -5,12 +9,36 @@ import { Team } from "../components/Team";
 
 
 export default function HomePage() {
+  const router = useRouter();
+  const [canRender, setCanRender] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      router.replace("/login");
+      return;
+    }
+
+    setCanRender(true);
+  }, [router]);
+
+  if (!canRender) {
+    return null;
+  }
+
   return (
     <>
-      <Hero/>
-      <CatalogGrid/>
-      <PromoSection/>
-      <Team/>
+      <section id="inicio">
+        <Hero />
+      </section>
+      <section id="catalogo">
+        <CatalogGrid />
+      </section>
+      <PromoSection />
+      <section id="nosotros">
+        <Team />
+      </section>
     </>
   );
 }
